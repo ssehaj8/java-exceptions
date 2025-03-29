a29034888874df0288394fd8e2c613dedb6379de
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-29 14:20:04 +0530

    [Sehajpreet] Add:Java program that reads a file named. If the file does not exist, handle the IOException

---
+++ src/main/java/com/bridgelabz/javaExceptions/FileReader.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.javaExceptions;
+
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader;
+
+
+
+public class FileReader {
+    public static void main(String[] args){
+        String sourcefile="C:/Users/sehajpreet/Desktop/bridgelabz-workspace/java-exceptions/src/main/java/com/bridgelabz/javaExceptions/input.txt";
+       try{
+           BufferedReader reader= new BufferedReader(new java.io.FileReader(sourcefile));
+           String line;
+           while((line=reader.readLine())!= null){
+               System.out.println(line);
+           }
+       }
+       catch (IOException e){
+           System.out.println("File not found");
+       }
+    }
+}
